c0b7bb7d7f685f991c7cd80eeb58a12c88df58ca
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-05-31 04:05:09 +0800

    get summary

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
     const [article, setArticle] = useState({
@@ -8,7 +9,23 @@ const Demo = () => {
         summary: "",
     });
 
-    const handleSearch = async () => {};
+    const [allArticles, setAllArticles] = useState([]);
+
+    const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+
+        const { data } = await getSummary({ articleUrl: article.url });
+
+        if (data?.summary) {
+            const newArticle = { ...article, summary: data.summary };
+            const updatedAllArticles = [newArticle, ...allArticles];
+
+            setArticle(newArticle);
+            setAllArticles(updatedAllArticles);
+        }
+    };
     const handleChange = (e) => {
         setArticle({
             ...article,
